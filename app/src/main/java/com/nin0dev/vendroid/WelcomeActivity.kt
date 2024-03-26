023e7462558d675963d71b56cd957150c1e5d284
nin0dev <support@nin0dev.com>
2024-03-26 07:33:15 -0400

    Added status/navbar transparency

--- app/src/main/java/com/nin0dev/vendroid/WelcomeActivity.kt
+++ app/src/main/java/com/nin0dev/vendroid/WelcomeActivity.kt
@@ -1,11 +1,21 @@
 package com.nin0dev.vendroid
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.statusBarColor = Color.TRANSPARENT
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+        window.navigationBarColor = Color.TRANSPARENT
+
         setContentView(R.layout.activity_welcome)
     }
 }
