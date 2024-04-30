6c379259a51da811c70dd2e4539ccb93b7c6c38b
nin0dev <personal@nin0dev.com>
2024-04-30 07:38:48 -0400

    Fix #7

--- app/src/main/java/com/nin0dev/vendroid/SettingsActivity.kt
+++ app/src/main/java/com/nin0dev/vendroid/SettingsActivity.kt
@@ -1,8 +1,11 @@
 package com.nin0dev.vendroid
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Color
+import android.opengl.Visibility
 import android.os.Bundle
+import android.service.voice.VoiceInteractionSession.VisibleActivityCallback
 import android.view.View
 import android.view.WindowManager
 import android.widget.CheckBox
@@ -13,17 +16,33 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import com.google.android.material.materialswitch.MaterialSwitch
+import com.google.android.material.radiobutton.MaterialRadioButton
+import com.google.android.material.textfield.TextInputEditText
 
 class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("CutPasteId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        val sPrefs = getSharedPreferences("settings", Context.MODE_PRIVATE)
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
         window.statusBarColor = Color.TRANSPARENT
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
         window.navigationBarColor = Color.TRANSPARENT
 
         setContentView(R.layout.activity_settings)
+
+        findViewById<MaterialSwitch>(R.id.check_vendroid_updates).isChecked = sPrefs.getBoolean("checkVendroidUpdates", false)
+        when (sPrefs.getString("discordBranch", "stable")) {
+            "stable" -> findViewById<MaterialRadioButton>(R.id.stable).isChecked = true
+            "ptb" -> findViewById<MaterialRadioButton>(R.id.ptb).isChecked = true
+            "canary" -> findViewById<MaterialRadioButton>(R.id.canary).isChecked = true
+        }
+        if(sPrefs.getString("vencordLocation", "")?.isNotBlank() == true) {
+            findViewById<CheckBox>(R.id.allow_custom_location).isChecked = true
+            val devbuildField = findViewById<TextInputEditText>(R.id.custom_location)
+            devbuildField.visibility = View.VISIBLE
+            devbuildField.setText(sPrefs.getString("vencordLocation", ""))
+        }
 
         val devbuildCheckbox = findViewById<CheckBox>(R.id.allow_custom_location)
         devbuildCheckbox.setOnClickListener {
@@ -41,11 +60,11 @@ class SettingsActivity : AppCompatActivity() {
             }
             else {
                 findViewById<EditText>(R.id.custom_location).visibility = View.GONE
+                findViewById<EditText>(R.id.custom_location).setText("")
             }
         }
 
         findViewById<ExtendedFloatingActionButton>(R.id.save_settings).setOnClickListener {
-            val sPrefs = getSharedPreferences("settings", Context.MODE_PRIVATE)
             val editor = sPrefs.edit()
 
             editor.putBoolean("checkVendroidUpdates", findViewById<MaterialSwitch>(R.id.check_vendroid_updates).isChecked)
