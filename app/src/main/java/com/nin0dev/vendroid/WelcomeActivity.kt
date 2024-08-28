b3544cdee2f7e43f414732570693b95d383add51
nin0dev <personal@nin0.dev>
2024-08-28 10:43:59 -0400

    removed devbuild settings from welcome screen

--- app/src/main/java/com/nin0dev/vendroid/WelcomeActivity.kt
+++ app/src/main/java/com/nin0dev/vendroid/WelcomeActivity.kt
@@ -27,25 +27,6 @@ class WelcomeActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_welcome)
 
-        val devbuildCheckbox = findViewById<CheckBox>(R.id.allow_custom_location)
-        devbuildCheckbox.setOnClickListener {
-            if (devbuildCheckbox.isChecked) {
-                MaterialAlertDialogBuilder(this)
-                        .setTitle("Warning")
-                        .setMessage("If you set a custom location, you will be loading and injecting Vencord from a different location. This feature is meant for developers ONLY. Never edit this setting if someone else asked you to, or if you don't know what you're doing! If you do set a custom location, you will not be able to ask for support in the Vencord support channel or in this project's issues. Are you sure you want to continue?")
-                        .setNegativeButton(resources.getString(R.string.no)) { _, _ ->
-                            devbuildCheckbox.isChecked = false
-                        }
-                        .setPositiveButton(resources.getString(R.string.yes)) { _, _ ->
-                            findViewById<EditText>(R.id.custom_location).visibility = VISIBLE
-                        }
-                        .show()
-            }
-            else {
-                findViewById<EditText>(R.id.custom_location).visibility = GONE
-            }
-        }
-
         findViewById<ExtendedFloatingActionButton>(R.id.save_settings).setOnClickListener {
             val sPrefs = getSharedPreferences("settings", Context.MODE_PRIVATE)
             val editor = sPrefs.edit()
@@ -54,7 +35,6 @@ class WelcomeActivity : AppCompatActivity() {
             if (findViewById<RadioButton>(R.id.stable).isChecked) editor.putString("discordBranch", "stable")
             if (findViewById<RadioButton>(R.id.ptb).isChecked) editor.putString("discordBranch", "ptb")
             if (findViewById<RadioButton>(R.id.canary).isChecked) editor.putString("discordBranch", "canary")
-            if (findViewById<CheckBox>(R.id.allow_custom_location).isChecked && findViewById<EditText>(R.id.custom_location).text.isNotBlank()) editor.putString("vencordLocation", findViewById<EditText>(R.id.custom_location).text.toString())
 
             editor.apply()
             finish()
