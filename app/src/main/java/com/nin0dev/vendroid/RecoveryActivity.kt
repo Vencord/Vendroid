fb26704fd555aa67d5351bf695c808d5981210b9
nin0 <personal@nin0.dev>
2025-08-12 18:25:21 -0400

    add force updating

--- app/src/main/java/com/nin0dev/vendroid/RecoveryActivity.kt
+++ app/src/main/java/com/nin0dev/vendroid/RecoveryActivity.kt
@@ -5,16 +5,8 @@ import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.view.WindowManager
-import android.webkit.CookieManager
-import android.webkit.CookieSyncManager
-import android.webkit.WebView
-import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.card.MaterialCardView
-import com.google.android.material.snackbar.Snackbar
 
 class RecoveryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,6 +27,14 @@ class RecoveryActivity : AppCompatActivity() {
             val sPrefs = getSharedPreferences("settings", Context.MODE_PRIVATE)
             val e = sPrefs.edit()
             e.putBoolean("safeMode", true)
+            e.apply()
+            finish()
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+        findViewById<MaterialCardView>(R.id.force_update).setOnClickListener {
+            val sPrefs = getSharedPreferences("settings", Context.MODE_PRIVATE)
+            val e = sPrefs.edit()
+            e.putInt("lastMajorUpdateThatUserHasUpdatedVencord", 0)
             e.apply()
             finish()
             startActivity(Intent(this, MainActivity::class.java))
